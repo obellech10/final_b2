22fc5e6048d3c1baeb813f6af50231f271eb5b71
Oscar Bellechasse <obellech10@gmail.com>
2019-07-26 16:40:02 -0600

    Add method sorted_grades_by_course

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@ class Course < ApplicationRecord
 
   has_many :student_courses
   has_many :students, through: :student_courses
+
+  def sorted_grades_by_course
+    self.student_courses.order(grade: :desc)
+  end
 end
